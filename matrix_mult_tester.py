1ce292e5f38b5a4631722ac5f0593e0e3b108d14
MikeTerekhov <113790396+MikeTerekhov@users.noreply.github.com>
2023-01-24 10:29:26 -0500

    name change

--- matrix_mult_tester.py
+++ matrix_mult_tester.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def matrix_multiplier(mat1, mat2):
     row_mat1, col_mat1 = np.shape(mat1)
-    row_mat2, col_mat2 = np.shape(mat2)
+    row_mat2, col_mat2 = np.shape(mat2)        
 
     if col_mat1 != row_mat2:
         raise ValueError("Matrix dimensions are incompatible")
@@ -14,6 +14,4 @@ def matrix_multiplier(mat1, mat2):
     if row_mat2 | col_mat2 == 0:
         raise ValueError("The second matrix entered is not a matrix")
 
-
-print("hi")  
-print("byee")     
+  
