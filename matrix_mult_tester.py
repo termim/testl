9fbba486989cbd6a992fad429023a0537c4d2762
MikeTerekhov <113790396+MikeTerekhov@users.noreply.github.com>
2023-02-01 18:52:33 -0500

    logging addition

--- matrix_mult_tester.py
+++ matrix_mult_tester.py
@@ -1,17 +1,22 @@
 import numpy as np
 import pytest
 import cProfile
+import logging
+
+logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
+logging.info('This program was utilized')
 
 a = [[1, 1, 1], [2, 2, 'A'], [1, 1, 1]]
+# a = [0]
 
 b = [[1, 1, 1], [2, 2, 2], [1, 1, 1]]
 
 
 def matrix_multiplier_tester(mat1, mat2):
     row_mat1, col_mat1 = np.shape(mat1)
-    print(row_mat1, col_mat1)
+    # print(row_mat1, col_mat1)
     row_mat2, col_mat2 = np.shape(mat2)   
-    print(row_mat2, col_mat2)     
+    # print(row_mat2, col_mat2)     
 
     if col_mat1 != row_mat2:
         print("Matrix dimensions are incompatible")
@@ -21,6 +26,7 @@ def matrix_multiplier_tester(mat1, mat2):
 
     if (row_mat2 or col_mat2 == 0) == 1:
         print("The second matrix entered is not a matrix")  
+
     for row in mat1:
         for value in row:
             if not isinstance(value, int):
@@ -46,5 +52,5 @@ def main():
 
 
 if __name__ == '__main__':
-    #cProfile.run('main()')
+    cProfile.run('main()')
     matrix_multiplier_tester(a, b)
