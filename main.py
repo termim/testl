d25822136b31440bc88cfbb6812cf324f43588cc
MikeTerekhov <113790396+MikeTerekhov@users.noreply.github.com>
2023-02-02 22:56:31 -0500

    adding pytest tests and checking matrix dimensions in main

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ import cProfile
 import logging
 import tracemalloc
 from matrix_mult_tester import matrix_multiplier_tester
-from obtain_input import obtain_input
+from obtain_input import obtain_input_mat
 from matrix_multiplier import matrix_multiplier
 from print_mat import print_mat
+import sys
 
 logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
 logging.info('This program was utilized')
@@ -13,9 +14,15 @@ logging.info('This program was utilized')
 
 def main():
     tracemalloc.start()
-    matrix1 = obtain_input()
-    matrix2 = obtain_input()
-    assert (matrix1 == matrix2)
+    print("Enter matrix 1 : ")
+    matrix1 = obtain_input_mat()
+    print("Enter matrix 2 : ")
+    matrix2 = obtain_input_mat()
+    col_1 = len(matrix1[0])
+    row_2 = len(matrix2)
+    if col_1 != row_2:
+        print("Matrix dimensions do not allow for multiplication!")
+        sys.exit()
     print_mat(matrix1)
     print_mat(matrix2)
     matrix_multiplier_tester(matrix1)
---
+++ test_examples.py
@@ -0,0 +1,63 @@
+import io
+import pytest
+
+from matrix_multiplier import matrix_multiplier
+from obtain_input import obtain_input_mat
+
+
+def test_matrix_multiplier_1():
+
+    m1 = [[1, 0], [0, 1]]
+    m2 = [[1, 0], [0, 1]]
+    r = matrix_multiplier(m1, m2)
+    assert r == [[1, 0], [0, 1]]
+
+
+def test_matrix_multiplier_1_1():
+
+    m1 = [[1, 1], [1, 1]]
+    m2 = [[1, 1], [1, 1]]
+    r = matrix_multiplier(m1, m2)
+    assert r == [[2, 2], [2, 2]]
+
+
+def test_matrix_multiplier_2():
+
+    m1 = [[1, 2], [3, 4]]
+    m2 = [[1, 2], [3, 4]]
+    r = matrix_multiplier(m1, m2)
+    assert r == [[7, 10], [15, 22]]
+
+
+def test_good_matrix_input(monkeypatch):
+    """
+    Verify that proper integer input is accepted.
+    """
+
+    # monkeypatch the "sys.stdin", so that it returns
+    # matrix dimensions and values (all separated by new line)
+    stdin = io.StringIO("2\n2\n1\n2\n3\n4\n")
+    monkeypatch.setattr('sys.stdin', stdin)
+
+    # go about using input() like you normally would
+    # it uses sys.stdin under the hood:
+    m = obtain_input_mat()
+    assert m == [[1, 2], [3, 4]]
+
+
+def test_bad_matrix_input(monkeypatch):
+    """
+    Verify that non integer input is not accepted.
+    """
+
+    # monkeypatch the "sys.stdin", so that it returns
+    # matrix dimensions and values (all separated by new line)
+    stdin = io.StringIO("2\n2\n1\nd\n3\n4\n")
+    monkeypatch.setattr('sys.stdin', stdin)
+
+    # go about using input() like you normally would
+    # it uses sys.stdin under the hood:
+    with pytest.raises(ValueError) as exc_info:
+        m = obtain_input_mat()
+    # assert exc_info.value.args[0] == 'some info'
+    assert str(exc_info.value) == 'some info'
